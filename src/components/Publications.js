0e3e530d00f30ef14ae35825c03334f9ac369506
Alexander <aksovius@naver.com>
2022-04-21 13:56:05 +0900

    a

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -7,16 +7,16 @@ const Publications = () => {
             <p className='title'>Publications</p>
             <ul className='list-table'>
                 <li>
-                    <p className='date'>2020-2022</p>
-                    <p className='list-primary'>title</p>
-                    <p className='list-secondary'>Master of Science in Engineering</p>
-                    <p className='list-tertiary'>Convergence Engineering for Future City</p>
+                    <p className='date'>2020</p>
+                    <p className='list-primary'>Kim A., Zhu Z., Lee K., Mun T., Kwon S.</p>
+                    <p className='list-secondary'>Development of User Interface for Temporary Structure Quality Evaluation System Based on Multiple Sensing and BIM Model</p>
+                    <p className='list-tertiary'>The 26th Winter Conference of Society for Computational Design and Engineering</p>
                 </li>
                 <li>
-                    <p className='date'>2008-2013</p>
-                    <p className='list-primary'> title</p>
-                    <p className='list-secondary'>Specialist degree</p>
-                    <p className='list-tertiary'>Industrial and Civil Engineering</p>
+                    <p className='date'>2020</p>
+                    <p className='list-primary'> Lee S., Kwon S., Jeong M., Hasan M., and Kim A.</p>
+                    <p className='list-secondary'>Automated On-Site Quality Inspection and Reporting Technology for Off-Site Construction (OSC)-based Precast Concrete Members.</p>
+                    <p className='list-tertiary'>2020 Proceedings of the 37th ISARC </p>
                 </li>
             </ul>
         </div>
