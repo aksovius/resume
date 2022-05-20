acc8eeb8edb882fb1c398b85ca75370a71ae3b19
Alexander Kim <aksovius@naver.com>
2022-05-20 23:27:28 +0900

    fix

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,13 +10,13 @@ const Experience = () => {
                 <ul className='list-table'>
                     <li>
                         <p className='date'>2021.08-2022.05</p>
-                        <p className='list-primary'><a href='http://deepinspection.ai/' target='_blank'>Deep Inspection Co. Ltd.</a></p>
+                        <p className='list-primary'><a href='https://deepinspection.ai/' target='_blank' rel="noreferrer">Deep Inspection Co. Ltd.</a></p>
                         <p className='list-secondary'>Research Engineer</p>
                         <p className='list-tertiary'>Developing a web based application</p>
                     </li>
                     <li>
                         <p className='date'>2018.01-2020.02</p>
-                        <p className='list-primary'><a href='http://www.medonica.com/html/main.html' target='_blank'>Medonica Co. Ltd.</a></p>
+                        <p className='list-primary'><a href='https://www.medonica.com/html/main.html' target='_blank' rel="noreferrer">Medonica Co. Ltd.</a></p>
                         <p className='list-secondary'>Medical Equipment Engineer</p>
                         <p className='list-tertiary'>Customer Service</p>
                     </li>
